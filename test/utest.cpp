d9736da889d2f661af04bc986200439fa6cc5611
Brian Buxton <bbuxton@umd.edu>
2017-04-26 18:38:44 -0500

    4: Got empty unit test working
    Key was that the type of the <test> xml element
    had to be the exact name of the test executable
    
    Also added empty Gazebo wold xml file
    Update .gitignore
    Fixed demo.launch name to match new project

---
+++ test/utest.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * @author Brian Buxton <bbux10@gmail.com>
+ * @brief unit test for TODO: Fill in brief
+ * @copyright BSD License
+ * Copyright (c) 2017 Brian Buxton
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+#include <ros/ros.h>
+#include <ros/service_client.h>
+#include <gtest/gtest.h>
+
+std::shared_ptr<ros::NodeHandle> nh;
+
+TEST(TestSuite, oneCoolTeset) {
+  EXPECT_TRUE(1 + 1 == 2);
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "walker_tests");
+  nh.reset(new ros::NodeHandle);
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
